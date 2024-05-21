49e5eafcacaccd6cfeeabe40f725679fc5bd6613
Ssymes-dev <samasymes@gmail.com>
2024-05-21 12:07:28 -0230

    added user controller, post test responds 404 but, connected

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const user_controller = require("../controllers/userController");
+
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get("/", user_controller.index);
+
+router.get("/user.create", user_controller.user_create_get);
+
+router.post("/user/create", user_controller.user_create_post);
+
+router.get("/user/:id/delete", user_controller.user_delete_get);
+
+router.post("/user/:id/delete", user_controller.user_delete_post);
+
+router.get("/user/:id/update", user_controller.user_update_get);
+
+router.post("/user/:id/update", user_controller.user_update_post);
+
+router.get("/user/:id", user_controller.user_detail);
+
+router.get("/user", user_controller.user_list);
 
 module.exports = router;
