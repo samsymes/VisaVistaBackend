8f9b108e1bd78d92f90e76b48cd8b4db4b86d7ac
Ssymes-dev <samasymes@gmail.com>
2024-05-29 15:31:22 -0230

    creating database connection

--- src/api-controllers/routes/user-account.controller.ts
+++ src/api-controllers/routes/user-account.controller.ts
@@ -1,12 +1,24 @@
 import express, { Request, Response, Router } from "express";
-
+// import { db } from "../../modules/database";
 export const UserAccountRouter = express.Router();
+UserAccountRouter.get("/items", (req: Request, res: Response) => {
+  res.send("Hello from UserAccountRouter!");
+  // db.query("SELECT * FROM test", (error: any, results: any) => {
+  //   if (error) throw error;
 
-UserAccountRouter.get("/login", (req: Request, res: Response) => {
-  res.send("Login");
+  //   res.status(200).json(results);
 });
 
-UserAccountRouter.post("/register", (req: Request, res: Response) => {
-  console.log("Request", req);
-  res.send("post recieved from server");
-});
+// UserAccountRouter.post("/items", (req: Request, res: Response) => {
+//   const { name, description } = req.body;
+
+//   db.query(
+//     "INSERT INTO items (name, description) VALUES (?, ?)",
+//     [name, description],
+//     (error: any, results: any) => {
+//       if (error) throw error;
+
+//       res.status(201).send(`Item added with ID: ${results.insertId}`);
+//     }
+//   );
+// });
---
+++ src/modules/database.ts
@@ -0,0 +1,14 @@
+import mysql from "mysql";
+
+export const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the database.");
+});
+module.exports = db;
