28ca2733a8ab8455edc3ba72572c4ee065784659
Dylan B <dylanbrine@gmail.com>
2024-06-03 07:40:58 -0300

    working locally

--- src/modules/database.ts
+++ src/modules/database.ts
@@ -1,14 +1,17 @@
-import mysql from "mysql";
+import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
+  host: 'database-test.cxgaqgqcktq1.ca-central-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'dbpassword',
 });
 
 db.connect((err) => {
   if (err) throw err;
-  console.log("Connected to the database.");
+  console.log('Connected to the database.');
 });
 module.exports = db;
