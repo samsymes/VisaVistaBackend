5d02d151018cfb4e0a12564c76791756a7f90a7a
Ssymes-dev <samasymes@gmail.com>
2024-09-01 14:47:09 -0400

    rest country service refactor finished

--- src/services/rest-countries.service.ts
+++ src/services/rest-countries.service.ts
@@ -7,15 +7,24 @@ export default class RestCountriesService {
       const response = await fetch(`${this.apiUrl}/${toCountry}`);
       const countryInfo = await response.json();
       console.log(countryInfo);
+
       if (countryInfo) {
         const countryInfoObj = countryInfo.map((country: any) => {
+          const currencies = Object.values(country.currencies)
+            .map((currency: any) => currency.name)
+            .join(", ");
+          const symbols = Object.values(country.currencies)
+            .map((currency: any) => currency.symbol)
+            .join(", ");
+          const languages = Object.values(country.languages).join(", ");
           return {
             name: country.name.common,
             capital: country.capital,
-            currencies: country.currencies, //[object Object] does not work
+            currencies,
+            symbols,
             timeZones: country.timezones.join(", "),
             population: country.population.toLocaleString(),
-            languages: Object.values(country.languages).join(", "),
+            languages,
             lat: country.latlng[0],
             lng: country.latlng[1],
           };
